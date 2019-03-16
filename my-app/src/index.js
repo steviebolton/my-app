93b3f7743a691b4cc4f5b92e5f71e1ba08380ef1
Steve Bolton <steviebolton@gmail.com>
2019-03-16 19:55:43 +0100

    constructor added to the square class to initialize the state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
-
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" onClick={function() { alert('click'); }}>
-          {this.props.value}
-        </button>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
   }
+
+  render() {
+    return (
+      <button className="square" onClick={() => alert('click')}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
   
   class Board extends React.Component {
     renderSquare(i) {
