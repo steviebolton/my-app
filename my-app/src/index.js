3dd62f9d3525cce045779513a9116aea5a5bcc08
Steve Bolton <steviebolton@gmail.com>
2019-03-16 19:39:59 +0100

    add onclick function which triggers an alert in the browser

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={function() { alert('click'); }}>
           {this.props.value}
         </button>
       );
