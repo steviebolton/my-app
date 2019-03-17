33e2dadf062cbf0ae38b836e296f9542607e5a6f
Steve Bolton <steviebolton@gmail.com>
2019-03-17 10:04:24 +0100

    square function updated with set state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,12 +12,21 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button 
+      className="square" 
+      onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
+
+
+
+
+
+
+
   
   class Board extends React.Component {
     renderSquare(i) {
